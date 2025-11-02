e801a70f3923c207c793a0668547f456adddf417
Peter Jun Koh <gopeterjun@naver.com>
2025-11-02 22:01:17 +0900

    first draft of 'binary tree' ADT; there is a bug in 'inorder()' output, however

---
+++ module-3-ADT-List-and-Binary-Trees/binary_tree.c
@@ -0,0 +1,63 @@
+/*
+ * The ADT Binary Tree
+ * by Kate Programmer
+ * September 29, 2019
+ * 
+ * This material is also covered in <A Book on C> chapter 10.8
+ * Binary Trees are useful because searches through notes can be done
+ * in logarithmic time.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char DATA;
+typedef struct node {
+    DATA d;
+    struct node* left;
+    struct node* right;
+} NODE;
+typedef NODE* BTREE;  // Binary Tree is a pointer of type NODE
+
+void inorder(BTREE root) {
+    // in-order tree traversal
+    if (root != NULL) {
+        inorder(root -> left);  // recur to the left
+        printf("%c", root -> d);
+        inorder(root -> right); // recur to the right
+        printf("%c", root -> d);
+    }
+}
+
+BTREE new_node(void) {
+    return malloc(sizeof(NODE));
+}
+
+BTREE init_node(DATA d1, BTREE p1, BTREE p2) {
+    // initialize DATA, left pointer and right pointer
+    BTREE t;
+    t = new_node();
+    t -> d = d1;
+    t -> left = p1;
+    t -> right = p2;
+    return t;  // return NODE pointer
+}
+
+BTREE create_tree(DATA a[], int i, int size) {
+    if (i >= size) {
+        return NULL;
+    } else {
+        return init_node(a[i],
+                         create_tree(a, 2*i + 1, size),
+                         create_tree(a, 2*i + 2, size));
+    }
+}
+
+int main() {
+    char d[] = {'a', 'b', 'c', 'd', 'e'};
+    BTREE b;
+    b = create_tree(d, 0, 5);
+    inorder(b);  // should print 'd b e a c'
+    printf("\n\n");
+    return 0;
+}
